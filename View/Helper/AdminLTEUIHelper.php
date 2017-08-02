ffb15e30ddb5107a6ab2b0a60ec5af3c45bf81d5
Sergio Hernandez Reyes <sergio.hdzreyes@gmail.com>
2017-08-02 18:12:51 -0500

    Se limpia variable que mantiene el contenido de los tabs al utilizarlos de manera multiple

--- View/Helper/AdminLTEUIHelper.php
+++ View/Helper/AdminLTEUIHelper.php
@@ -7,9 +7,16 @@ class AdminLTEUIHelper extends HtmlHelper
 
     public $_contentBlockTabs = array();
 
-    public function tabStart($tab_title)
+    public $_tabOptions = array();
+
+    public function tabStart($tab_title, $options = array())
     {
+        $default_options = array(
+            'class' => '',
+            'id', ''
+        );
         $this->_currentTab = $tab_title;
+        $this->_tabOptions[$tab_title] = array_merge($default_options, $options);
         ob_start();
         return null;
     }
@@ -24,13 +31,22 @@ class AdminLTEUIHelper extends HtmlHelper
             $idx = 1;
             foreach ($this->_contentBlockTabs as $header => $tab_content) {
                 $header_slugged = Inflector::slug($header) . '_' . $idx;
-                $li_tabs_headers[] = '<li class="' . ($idx == 1 ? 'active' : '') . '"><a href="#' . $header_slugged . '" data-toggle="tab">' . $header . '</a></li>';
+                $params = '';
+                foreach ($this->_tabOptions[$header] as $options => $value){
+                    if(($options != 'id') && ($options != 'class') && (!empty($value)))
+                        $params .= $options . '="' .$value. '"';
+                }
+
+                $li_tabs_headers[] = '<li class="' . ($idx == 1 ? 'active' : '') . ' '.$this->_tabOptions[$header]['class'].'" id="'.$this->_tabOptions[$header]['id'].'" '.$params.'>
+                                          <a href="#' . $header_slugged . '" data-toggle="tab">' . $header . '</a>
+                                      </li>';
                 $divs_tab_content[] = '<div class="tab-pane ' . ($idx == 1 ? 'active' : '') . '" id="' . $header_slugged . '">' . $tab_content . '</div>';
                 $idx++;
             }
             $li_tabs_html = join('', $li_tabs_headers);
             $tab_content_html = join('', $divs_tab_content);
             $html = '<div class="nav-tabs-custom"><ul class="nav nav-tabs">' . $li_tabs_html . '</ul><div class="tab-content">' . $tab_content_html . '</div></div>';
+            $this->_contentBlockTabs = array();
         }
 
         print $html;
