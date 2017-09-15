0852b7df2085f23b56107569c9ebc8c1371d87f1
Enrique Martinez <enmaca@users.noreply.github.com>
2017-09-15 09:55:09 -0500

    * Fix Notice in AdminLTEUIHelper.php

--- View/Helper/AdminLTEUIHelper.php
+++ View/Helper/AdminLTEUIHelper.php
@@ -13,7 +13,8 @@ class AdminLTEUIHelper extends HtmlHelper
     {
         $default_options = array(
             'class' => '',
-            'id', ''
+            'id',
+            ''
         );
         $this->_currentTab = $tab_title;
         $this->_tabOptions[$tab_title] = array_merge($default_options, $options);
@@ -32,23 +33,23 @@ class AdminLTEUIHelper extends HtmlHelper
             foreach ($this->_contentBlockTabs as $header => $tab_content) {
                 $header_slugged = Inflector::slug($header) . '_' . $idx;
                 $params = '';
-                foreach ($this->_tabOptions[$header] as $options => $value){
-                    if(($options != 'id') && ($options != 'class') && (!empty($value)))
-                        $params .= $options . '="' .$value. '"';
+                foreach ($this->_tabOptions[$header] as $options => $value) {
+                    if (($options != 'id') && ($options != 'class') && (! empty($value)))
+                        $params .= $options . '="' . $value . '"';
                 }
-
-                $li_tabs_headers[] = '<li class="' . ($idx == 1 ? 'active' : '') . ' '.$this->_tabOptions[$header]['class'].'" id="'.$this->_tabOptions[$header]['id'].'" '.$params.'>
+                
+                $li_tabs_headers[] = '<li class="' . ($idx == 1 ? 'active' : '') . ' ' . ((! empty($this->_tabOptions[$header]['class'])) ? $this->_tabOptions[$header]['class'] : '') . '" ' . ((! empty($this->_tabOptions[$header]['id'])) ? 'id="' . $this->_tabOptions[$header]['id'] : '') . '" ' . $params . '>
                                           <a href="#' . $header_slugged . '" data-toggle="tab">' . $header . '</a>
                                       </li>';
                 $divs_tab_content[] = '<div class="tab-pane ' . ($idx == 1 ? 'active' : '') . '" id="' . $header_slugged . '">' . $tab_content . '</div>';
-                $idx++;
+                $idx ++;
             }
             $li_tabs_html = join('', $li_tabs_headers);
             $tab_content_html = join('', $divs_tab_content);
             $html = '<div class="nav-tabs-custom"><ul class="nav nav-tabs">' . $li_tabs_html . '</ul><div class="tab-content">' . $tab_content_html . '</div></div>';
             $this->_contentBlockTabs = array();
         }
-
+        
         print $html;
     }
 }
