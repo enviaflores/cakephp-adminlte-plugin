4c0323076d5439d51529b8fb80e3880a31a3987b
alex1403ia <alex1403ia@gmail.com>
2019-01-04 18:17:16 -0600

    Updated select2 plugin to 4.0.5, solves the overlap issues when a long text is selected and the "X" symbol do not works

--- webroot/js/select2/i18n/et.js
+++ webroot/js/select2/i18n/et.js
@@ -1,4 +1,3 @@
-/*
- Select2 4.0.0 | https://github.com/select2/select2/blob/master/LICENSE.md */
-(function(){if(jQuery&&jQuery.fn&&jQuery.fn.select2&&jQuery.fn.select2.amd)var c=jQuery.fn.select2.amd;return c.define("select2/i18n/et",[],function(){return{inputTooLong:function(a){a=a.input.length-a.maximum;var b="Sisesta "+a+" t\u00e4ht";return 1!=a&&(b+="e"),b+=" v\u00e4hem",b},inputTooShort:function(a){a=a.minimum-a.input.length;var b="Sisesta "+a+" t\u00e4ht";return 1!=a&&(b+="e"),b+=" rohkem",b},loadingMore:function(){return"Laen tulemusi\u2026"},maximumSelected:function(a){var b="Saad vaid "+
-a.maximum+" tulemus";return 1==a.maximum?b+="e":b+="t",b+=" valida",b},noResults:function(){return"Tulemused puuduvad"},searching:function(){return"Otsin\u2026"}}}),{define:c.define,require:c.require}})();
+/*! Select2 4.0.5 | https://github.com/select2/select2/blob/master/LICENSE.md */
+
+(function(){if(jQuery&&jQuery.fn&&jQuery.fn.select2&&jQuery.fn.select2.amd)var e=jQuery.fn.select2.amd;return e.define("select2/i18n/et",[],function(){return{inputTooLong:function(e){var t=e.input.length-e.maximum,n="Sisesta "+t+" täht";return t!=1&&(n+="e"),n+=" vähem",n},inputTooShort:function(e){var t=e.minimum-e.input.length,n="Sisesta "+t+" täht";return t!=1&&(n+="e"),n+=" rohkem",n},loadingMore:function(){return"Laen tulemusi…"},maximumSelected:function(e){var t="Saad vaid "+e.maximum+" tulemus";return e.maximum==1?t+="e":t+="t",t+=" valida",t},noResults:function(){return"Tulemused puuduvad"},searching:function(){return"Otsin…"}}}),{define:e.define,require:e.require}})();
